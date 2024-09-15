bdf4920ae7f597f18b713bf678e8881451dc73b9
wynagito <2234774155@qq.com>
2024-09-15 15:59:41 +0800

    llll

--- algorithmzuo_recording/6/t03_find_peak_element.cpp
+++ algorithmzuo_recording/6/t03_find_peak_element.cpp
@@ -10,38 +10,42 @@
 
 using namespace std;
 
-int findPeakElement(vector<int>& arr)
+class Solution
 {
-    int n = arr.size();
-    if (arr.size() == 1)
+public:
+    int findPeakElement(vector<int> &arr)
     {
-        return 0;
-    }
-    if (arr[0] > arr[1])
-    {
-        return 0;
-    }
-    if (arr[n - 1] > arr[n - 2])
-    {
-        return n - 1;
-    }
-    int l = 1, r = n - 2, m = 0, ans = -1;
-    while (l <= r)
-    {
-        m = (l + r) / 2;
-        if (arr[m - 1] > arr[m])
+        int n = arr.size();
+        if (arr.size() == 1)
         {
-            r = m - 1;
+            return 0;
         }
-        else if (arr[m] < arr[m + 1])
+        if (arr[0] > arr[1])
         {
-            l = m + 1;
+            return 0;
         }
-        else
+        if (arr[n - 1] > arr[n - 2])
         {
-            ans = m;
-            break;
+            return n - 1;
         }
+        int l = 1, r = n - 2, m = 0, ans = -1;
+        while (l <= r)
+        {
+            m = (l + r) / 2;
+            if (arr[m - 1] > arr[m])
+            {
+                r = m - 1;
+            }
+            else if (arr[m] < arr[m + 1])
+            {
+                l = m + 1;
+            }
+            else
+            {
+                ans = m;
+                break;
+            }
+        }
+        return ans;
     }
-    return ans;
-}
+};
